d91900158cbce3d457641c30b47f6ddd796afe8e
Stephan Multani <stephan.multani@pm.me>
2023-04-19 20:45:05 +0200

    Formatted all files from day 001 and 002

---
+++ 100Days/Day001/E1-Printing.py
@@ -0,0 +1,16 @@
+# Exercise 1 - Printing
+"""
+Instructions:
+Write a program that prints some notes.
+
+Example Output:
+After you have written your code, you should run your program and it should print the following:
+Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')
+"""
+
+# Exercise 1 - Solution
+print('Day 1 - Python Print Function')
+print('The function is declared like this:')
+print("print('what to print')")
---
+++ 100Days/Day001/E2-Debugging.py
@@ -0,0 +1,24 @@
+# Exercise 2 - Debugging Practice
+"""
+Instructions:
+Look at the code. There are errors in all of the lines of code. Fix the code so that it runs without errors.
+
+Example Output:
+When you run your program, it should print the following:
+Day 1 - String Manipulation
+String Concatenation is done with the "+" sign.
+e.g. print("Hello " + "world")
+New lines can be created with a backslash and n.
+"""
+
+# Given Code 
+#print(Day 1 - String Manipulation")
+#print("String Concatenation is done with the "+" sign.")
+#  print('e.g. print("Hello " + "world")')
+#print(("New lines can be created with a backslash and n.")
+
+# Exercise 2 - Solution
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
