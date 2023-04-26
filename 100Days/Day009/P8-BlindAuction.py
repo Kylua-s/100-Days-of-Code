3059fe1b54edc89e0a851679b525c4a9efbfc68c
Stephan Multani <stephan.multani@pm.me>
2023-04-26 18:38:54 +0200

    Completed Day 009

---
+++ 100Days/Day009/P8-BlindAuction.py
@@ -0,0 +1,30 @@
+# Project 8 - blind-auction
+"""
+Idea:
+Write a program that will be able to create an auction and invite bidders to participate by placing bids. The highest bidder will win. 
+"""
+
+# Project x - Solution
+import os
+import art
+
+print(art.logo)
+bidders = {}
+while (True):
+    name = input("What is your name?: ")
+    bid = input("What is your bid?: $")
+    bidders[name] = bid
+    others = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+    os.system('cls')
+    if others == 'no':
+        break
+
+highest_bid = 0
+name = ""
+for key in bidders:
+    new_bid = int(bidders[key])
+    if new_bid > highest_bid:
+        name = key
+        highest_bid = new_bid
+
+print(f"The winner is {name} with a bid of ${highest_bid}.")
