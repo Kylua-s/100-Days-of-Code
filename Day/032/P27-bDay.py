303fec7df5517f9903ea5181eb2b81f5afa1771e
Stephan Multani <stephan.multani@pm.me>
2023-05-23 18:23:59 +0200

    Day 27 re-upload

---
+++ Day/032/P27-bDay.py
@@ -0,0 +1,37 @@
+# Project 27 - bDay
+"""
+This project sends b-day wishes to your family and friend
+
+Note! For the code to work, you need to replace all the placeholders with your own details:
+MY_EMAIL, MY_PASSWORD, SMTP ADDRESS, and the birthdays.csv
+
+Due to privacy concerns, I have omitted my personal data.
+"""
+from datetime import datetime
+import pandas
+import random
+import smtplib
+
+MY_EMAIL = "YOUR EMAIL"
+MY_PASSWORD = "YOUR PASSWORD"
+
+today = datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letters/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
